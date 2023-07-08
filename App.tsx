9d4a883129618fc8d9ff43602a5ab1bb072b9885
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-08 17:10:31 -0300

    feat: Adiciona StatusBar personalizada

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
@@ -6,6 +6,12 @@ export default function App() {
 
   return (
     <View>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
       {
         fontsLoaded ? <Text>Hello world</Text> : <View />
       }
