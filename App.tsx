29e71ca2e163787998df5262075c1a533ab92f29
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-08 23:41:28 -0300

    feat: Adiciona o Loading com NativeBase

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,9 +16,7 @@ export default function App() {
         translucent
       />
 
-      {
-        fontsLoaded ? <Text>Hello world</Text> : <View />
-      }
+      <Loading />
     </NativeBaseProvider>
   );
 }
