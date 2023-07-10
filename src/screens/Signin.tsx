b5b3d9e62f74e8e13660f52d7e512cba35ac02d7
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-10 16:25:08 -0300

    feat: Adiciona variantes do Button componet

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -8,41 +8,61 @@ import { Button } from '@components/Button';
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg='gray.700' px={10}>
-      <Image
-        source={BackgroundImg}
-        alt='Pessoas treinando em bicicletas na academia'
-        resizeMode='contain'
-        position='absolute'
-      />
-
-      <Center my={24}>
-        <LogoSvg />
-
-        <Text color='gray.100' fontSize='sm'>
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-
-      <Center>
-        <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
-          Acesse sua conta
-        </Heading>
-
-        <Input
-          type='text'
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCapitalize='none'
-        />
-        <Input
-          type='password'
-          placeholder='Senha'
-          autoCapitalize='none'
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1}}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg='gray.700' px={10}>
+        <Image
+          source={BackgroundImg}
+          alt='Pessoas treinando em bicicletas na academia'
+          resizeMode='contain'
+          position='absolute'
         />
 
-        <Button title='Acessar'/>
-      </Center>
-    </VStack>
+        <Center my={24}>
+          <LogoSvg />
+
+          <Text color='gray.100' fontSize='sm'>
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            type='text'
+            placeholder='E-mail'
+            keyboardType='email-address'
+            autoCapitalize='none'
+          />
+          <Input
+            type='password'
+            placeholder='Senha'
+            autoCapitalize='none'
+          />
+
+          <Button title='Acessar'/>
+        </Center>
+
+        <Center mt={24}>
+          <Text
+            color='gray.100'
+            fontSize='sm'
+            mb={3}
+            fontFamily='body'
+          >
+            Ainda não tem acesso?
+          </Text>
+          <Button
+            title='Criar conta'
+            variant='outline'
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 };
