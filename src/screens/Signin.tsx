51d04e182b4fa7120898c08e319247119911f034
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-10 15:58:30 -0300

    feat: Adiciona o Input componet

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -3,9 +3,11 @@ import { VStack, Image, Text, Center, Heading } from 'native-base';
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
+import { Input } from '@components/Input';
+
 export function SignIn() {
   return (
-    <VStack flex={1} bg='gray.700'>
+    <VStack flex={1} bg='gray.700' px={10}>
       <Image
         source={BackgroundImg}
         alt='Pessoas treinando em bicicletas na academia'
@@ -25,6 +27,18 @@ export function SignIn() {
         <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
           Acesse sua conta
         </Heading>
+
+        <Input
+          type='text'
+          placeholder='E-mail'
+          keyboardType='email-address'
+          autoCapitalize='none'
+        />
+        <Input
+          type='password'
+          placeholder='Senha'
+          autoCapitalize='none'
+        />
       </Center>
     </VStack>
   );
