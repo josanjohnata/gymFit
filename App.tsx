280252e49ffef41aad4ff5e385c06fb2362fb48d
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-09 22:06:05 -0300

    feat: Adiciona o theme default com NativeBase

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, View } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
 
-      <Loading />
+      { fontsLoaded ? <View /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
