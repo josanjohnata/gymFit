75387aa2e6ac696cfa1cbbc856e03ab5d2ac0d8e
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-14 19:54:46 -0300

    feat: Finaliza o componente de History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Heading, VStack, SectionList } from "native-base";
+import { Heading, VStack, SectionList, Text, Center } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { HistoryCard } from "@components/HistoryCard";
@@ -41,6 +41,15 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={sectionExercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color='gray.100' textAlign='center'>
+            Não há exercícios registrados ainda.
+            {'\n'}
+            Vamos treinar hoje?
+          </Text>
+        )}
+        showsHorizontalScrollIndicator={false}
       />
       
     </VStack>
