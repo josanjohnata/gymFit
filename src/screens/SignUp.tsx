324a9f93f586e087e76bb8a97e2ac2b602b76c9a
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-17 20:33:07 -0300

    feat: Autenticando os formularios com o react-hook-form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { useForm, Controller } from 'react-hook-form';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -8,10 +9,16 @@ import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
 export function SignUp() {
+  const { control, handleSubmit } = useForm();
+
   const navigation = useNavigation();
 
   const handleGoBack = () => {
     navigation.goBack();
+  };
+
+  const handleSingUp = (data: any) => {
+    
   };
   
   return (
@@ -41,25 +48,66 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input
-            type='text'
-            placeholder='Nome'
+          <Controller
+            control={control}
+            name='name'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='text'
+                placeholder='Nome'
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
-          <Input
-            type='text'
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none'
+          <Controller
+            control={control}
+            name='email'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                type='text'
+                placeholder='E-mail'
+                keyboardType='email-address'
+                autoCapitalize='none'
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
-          <Input
-            type='password'
-            placeholder='Senha'
-            autoCapitalize='none'
+          <Controller
+            control={control}
+            name='password'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
 
-          <Button title='Criar e acessar'/>
+          <Controller
+            control={control}
+            name='password_confirm'
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder='Confirmar a senha'
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSingUp)}
+                returnKeyType='send'
+              />
+            )}
+          />
+
+          <Button
+            title='Criar e acessar'
+            onPress={handleSubmit(handleSingUp)}
+          />
         </Center>
 
         <Button
