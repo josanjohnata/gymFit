00b9eea2afcb34b0ef24329ccf02b99ce244f309
Josan Johnata <josan.pontes@meliuz.com.br>
2023-07-17 22:18:20 -0300

    feat: Adiciona validação das senhas

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -18,8 +18,10 @@ type FormDataProps = {
 };
 
 const signUpSchema = yup.object({
-  name: yup.string().required('Informe o nome'),
-  email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+  name: yup.string().required('Informe o nome.'),
+  email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
+  password: yup.string().required('Informe a senha.').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+  password_confirm: yup.string().required('Confirme a senha.').oneOf([yup.ref('password'), null], 'A confirmação da senha não confere.')
 });
 
 export function SignUp() {
@@ -103,6 +105,7 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password?.message}
               />
             )}
           />
@@ -118,6 +121,7 @@ export function SignUp() {
                 value={value}
                 onSubmitEditing={handleSubmit(handleSingUp)}
                 returnKeyType='send'
+                errorMessage={errors.password_confirm?.message}
               />
             )}
           />
@@ -131,7 +135,7 @@ export function SignUp() {
         <Button
           title='Voltar para o login'
           variant='outline'
-          mt={24}
+          mt={12}
           onPress={handleGoBack}
         />
       </VStack>
