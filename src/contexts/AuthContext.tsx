6fb3acb9c3e419a684d408c45e808329e7f111d4
Josan Johnata <josan.pontes@meliuz.com.br>
2023-08-14 12:36:20 -0300

    feat: Recuperando o token do storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 
-import { storageAuthTokeSave } from '@storage/storageAuthToken';
+import { storageAuthTokeGet, storageAuthTokeSave } from '@storage/storageAuthToken';
 import { storageUserGet, storageUserSave, storageUserRemove } from '@storage/storageUser';
 
 import { api } from '@services/api';
@@ -24,19 +24,20 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
-  const storageUserAndToken = async (userData: UserDTO, token: string) => {
+  const userAndTokenUpdate = async (userData: UserDTO, token: string) => {
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      setUser(userData);
+  }
+
+  const storageUserAndTokenSave = async (userData: UserDTO, token: string) => {
     try {
       setIsLoadingUserStorageData(true);
-
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     
       await storageUserSave(userData);
       await storageAuthTokeSave(token);
-      setUser(userData);
-
     } catch (error) {
       throw error;
-
     } finally {
       setIsLoadingUserStorageData(false);
     }
@@ -47,7 +48,9 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { data } = await api.post('/sessions', { email, password});
 
       if(data.user && data.token) {
-        storageUserAndToken(data.user, data.token);
+        await storageUserAndTokenSave(data.user, data.token);
+
+        userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
       throw error;
@@ -69,10 +72,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   const loadUserData = async () => {
     try {
-      const userLogged = await storageUserGet();
+      setIsLoadingUserStorageData(true);
 
-      if(userLogged) {
-        setUser(userLogged);
+      const userLogged = await storageUserGet();
+      const token = await storageAuthTokeGet();
+
+      if(token && userLogged) {
+        userAndTokenUpdate(userLogged, token);
       }
 
     } catch (error) {
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -5,3 +5,9 @@ import { AUTH_TOKEN__STORAGE } from '@storage/storageConfig';
 export async function storageAuthTokeSave(token: string) {
   await AsyncStorage.setItem(AUTH_TOKEN__STORAGE, token);
 }
+
+export async function storageAuthTokeGet() {
+  const token = await AsyncStorage.getItem(AUTH_TOKEN__STORAGE);
+
+  return token;
+}
